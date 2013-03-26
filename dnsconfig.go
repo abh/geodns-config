57b27a90ca1ce97daa9ce91b73d0a4cd7830abb3
Ask Bjørn Hansen <ask@develooper.com>
2013-03-26 02:25:13 -0700

    Make main program useful

--- dnsconfig.go
+++ dnsconfig.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 var VERSION string = "2.0.0"
 var gitVersion string
 
-func main() {
-
-}
+var (
+	zonesFile = flag.String("config", "config/zones.json", "zones.json configuration file")
+)
 
 func init() {
 	if len(gitVersion) > 0 {
@@ -18,4 +21,35 @@ func init() {
 
 	log.SetPrefix("geodns ")
 	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
+}
+
+func main() {
+
+	zones := new(Zones)
+
+	err := zones.LoadZonesConfig(*zonesFile)
+	if err != nil {
+		log.Printf("Could not open '%s': %s", *zonesFile, err)
+		os.Exit(2)
+	}
+
+	for _, zone := range zones.All() {
+
+		log.Printf("Building %s\n", zone.Name)
+		err = zone.LoadConfig()
+		if err != nil {
+			log.Printf("Could not load configuration for '%s': %s", zone.Name, err)
+			continue
+		}
+
+		js, err := zone.BuildJSON()
+		if err != nil {
+			log.Printf("Could not build DNS data for '%s': %s", zone.Name, err)
+			continue
+		}
+
+		fmt.Print(js)
+
+	}
+
 }
